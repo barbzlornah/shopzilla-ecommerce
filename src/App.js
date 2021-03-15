5b582ff0569e4d1d9baf4cab85331e8dea96376f
Barbara Masinde <barbzlornah@gmail.com>
2021-03-15 11:51:19 +0300

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import './default.scss';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <div className="main">
+      <Homepage/>
+    </div>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.scss';
+
+import Logo from './../../assets/logo.png';
+
+const Header = props => {
+    return (
+        <header className = 'header'>
+            <div className = 'wrap'>
+                <div className = 'logo'>
+                <img src={Logo} alt='shopzilla' />
+                </div>
+
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header
